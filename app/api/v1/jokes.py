bdee58bc438853dd8696a29d872bff1d62bcbdf8
Gary Bake <garybake@gmail.com>
2021-07-14 22:49:48 +0100

    Nearly templating

--- app/api/v1/jokes.py
+++ app/api/v1/jokes.py
@@ -1,9 +1,13 @@
 from typing import Any, List
 
 from fastapi import APIRouter
+from fastapi.responses import HTMLResponse
+
 
 from app import schemas
 from app.models import Joke
+from app.main import templates
+# import app
 
 router = APIRouter()
 
@@ -17,3 +21,8 @@ def read_jokes() -> Any:
     joke = Joke().get_new_joke()
 
     return [joke]
+
+#
+@router.get("/page", response_class=HTMLResponse)
+def render_page() -> Any:
+    return app.templates.TemplateResponse("page.html")
--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
 
 from app.api.v1 import api_router
 from app.core import settings
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
-app.include_router(api_router, prefix=settings.API_V1_STR)
 
+app.include_router(api_router, prefix=settings.API_V1_STR)
+templates = Jinja2Templates(directory="templates")
 if __name__ == "__main__":
     uvicorn.run('app.main:app', host="0.0.0.0", port=8000, reload=True)
