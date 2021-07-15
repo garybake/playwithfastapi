5d25b9df5f81b8bf0cba9976e0707479221436ac
Gary Bake <garybake@gmail.com>
2021-07-15 23:28:28 +0100

    bootstrap5 base template seems ok

--- app/main.py
+++ app/main.py
@@ -3,20 +3,25 @@ import logging
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
-# from fastapi.templating import Jinja2Templates
 
 from app.api.v1 import api_router
 from app.views import view_router
 from app.core import settings
 
+# Setup logging
 from app.core.log_conf import log_config
 dictConfig(log_config)
 logger = logging.getLogger('jokes-logger')
 
 logger.info('Starting Jokes App')
-app = FastAPI(title=settings.PROJECT_NAME)
 
+# Setup app
+app = FastAPI(title=settings.PROJECT_NAME)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Setup routes
 app.include_router(view_router)
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
